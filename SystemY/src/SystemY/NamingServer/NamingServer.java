2d8d984db9d81a75028fda438a52a323cba4ede2
Toon <Toon@143.169.223.162>
2016-10-25 12:40:32 +0200

    update opdrachtomschrijving

--- SystemY/src/SystemY/NamingServer/NamingServer.java
+++ SystemY/src/SystemY/NamingServer/NamingServer.java
@@ -8,3 +8,22 @@ public class NamingServer {
 	}
 
 }
+
+
+//Namingserver:
+//--> klasse nodelijst (bevat nodes)
+//==--> Methode JSON serialisatie
+//==--> Methode voor toevoegen nodes (hashing)
+//--> klasse node (bevat ip's, bestandsnamen en adressen, + attributen)
+//==--> 
+//--> abstracte klasse voor communicatie
+
+//Node:
+//--> klasse bestandenlijst
+//--> abstracte klasse voor communicatie
+
+
+//Toon: 192.168.1.1 (server)
+//Thijs: 192.168.1.2 (node)
+//Floris: 192.168.1.3 (node)
+//Matthias: 192.168.1.4 (node)
