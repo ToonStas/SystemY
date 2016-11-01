e1f57173fe50d0b55e41a4ed3c26ed59183148bf
Matthias <Matthias@ABTis-LT-01>
2016-11-01 20:30:34 +0100

    het werkt.

--- SystemY/src/SystemY/NamingServer/Node.java
+++ SystemY/src/SystemY/NamingServer/Node.java
@@ -4,28 +4,40 @@ import org.json.simple.JSONObject;
 
 public class Node {
 
-	public static JSONObject Node = new JSONObject();
+	public  JSONObject Node = new JSONObject();
 	private String name;
 	private String ipAdress;
 	private int hash;
 	
+	public Node (String name, String ipAdress)
+	{
+		this.name = name;
+		this.ipAdress = ipAdress;
+		calculatehash();
+		
+		Node.put("Name", this.name);
+		Node.put("IpAdress", this.ipAdress);
+		Node.put("hash", this.hash);
+	}
+	
 	public void setName (String name){
 		this.name = name;
-		Node.putIfAbsent("Name", this.name);
+		Node.replace("name",this.name);
 	}
 	public String getName (){
 		return(name);
 	}
 	public void setIpAdress (String ipAdress){
 		this.ipAdress = ipAdress;
-		Node.putIfAbsent("IpAdress", this.ipAdress);
+		Node.replace("ipAdress",this.ipAdress);
+
 	}
 	public String getIpAdress (){
 		return(ipAdress);
 	}
 	public void calculatehash(){
 		this.hash = 32768;
-		Node.putIfAbsent("Hash", this.hash);
+		Node.replace("hash", this.hash);
 	}
 	public int gethash(){
 		return(hash);
--- SystemY/src/SystemY/NamingServer/Nodelijst.java
+++ SystemY/src/SystemY/NamingServer/Nodelijst.java
@@ -3,29 +3,54 @@ package SystemY.NamingServer;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 
+
 public class Nodelijst {
 
 	public static JSONArray listofclients = new JSONArray();
+	public static List<Node> listofnodes = new ArrayList<Node>();
 	
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+		/*
 		JSONObject client1 = new JSONObject();
 		client1.put("Name", "Matthias");					//client name
 		client1.put("IPadress", "192.168.1.4");				//client Ipadress
-		client1.put("hash", new Integer(32768));			//client integer waarde bekomen door hash functie
+		client1.put("Hash", new Integer(32768));			//client integer waarde bekomen door hash functie
 		
 		listofclients.add(client1);
-		
+		*/
+		Nodelijst.addNode("Matthias","192.168.1.4");
+		Nodelijst.addNode("floris","192.168.1.2");
+		Nodelijst.addNode("Matthias","192.168.1.4");
 		Nodelijst.writeJSON();
 	}
 	
+	public static void addNode(String name, String ipaddr)
+	{
+		Node newnode = new Node(name,ipaddr);
+		listofnodes.add(newnode);
+	}
+	public static void removeNode(int place)
+	{
+		listofnodes.remove(place);
+	}
+	
+	@SuppressWarnings("unchecked")
 	public static void writeJSON()
 	{
+		//int size = listofnodes.size();
+		for(int i=0;i<listofnodes.size();i++)
+		{
+			System.out.printf("%d , %d\n",i,listofnodes.size());
+			listofclients.add(listofnodes.get(i).Node);
+		}
 		try
 		{
 			File file=new File("C:/TEMP/JSONFile.json");
