b05a57983fa9ee7fac485d958058faa0958213a8
Matthias <Matthias@ABTis-LT-01>
2016-11-01 12:43:35 +0100

    methodes om gegevens met JSON weg te schrijven. temp --> nog niet getest.

---
+++ SystemY/src/SystemY/NamingServer/Nodelijst.java
@@ -0,0 +1,49 @@
+package SystemY.NamingServer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+public class Nodelijst {
+
+	public static JSONArray listofclients = new JSONArray();
+	
+	public static void main(String[] args) 
+	{
+		JSONObject client1 = new JSONObject();
+		client1.put("Name", "Matthias");
+		client1.put("IPadress", new Integer(19216814));
+		
+		listofclients.add(client1);
+	}
+	
+	public static void writeJSON()
+	{
+		try
+		{
+			File file=new File("C:/TEMP/JSONFile.json");
+			file.createNewFile(); 
+			FileWriter fileWriter = new FileWriter(file);
+			System.out.println("Writing JSON object to file");
+			System.out.println("-----------------------");
+			System.out.print(listofclients);
+			fileWriter.write(listofclients.toJSONString());
+			fileWriter.flush();
+			fileWriter.close();
+		
+		
+		}
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void readJSON()
+	{
+		
+	}
+}
