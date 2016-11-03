33b3f8abb9b907003f1740a038355f1c970b2079
Matthias <Matthias@ABTis-LT-01>
2016-11-03 13:42:15 +0100

    reader afgewerkt. json update herbekijken.

--- SystemY/src/SystemY/NamingServer/Nodelijst.java
+++ SystemY/src/SystemY/NamingServer/Nodelijst.java
@@ -27,27 +27,32 @@ public class Nodelijst {
 		Nodelijst.addNode("floris","192.168.1.2");
 		Nodelijst.addNode("Matthias","192.168.1.4");
 		Nodelijst.writeJSON();
+		Nodelijst.readJSON();
 	}
 	
 	public static void addNode(String name, String ipaddr)
 	{
 		Node newnode = new Node(name,ipaddr);
 		listofnodes.add(newnode);
+		Nodelijst.updateJSON();
 	}
 	public static void removeNode(int place)
 	{
 		listofnodes.remove(place);
 	}
 	
+	public static void updateJSON()
+	{
+		listofclients.add(listofnodes.get(listofnodes.size()-1).Node);
+		/*for(int i=0;i<listofnodes.size();i++)
+		{
+			listofclients.add(listofnodes.get(i).Node);
+		}*/
+	}
+	
 	@SuppressWarnings("unchecked")
 	public static void writeJSON()
 	{
-		//int size = listofnodes.size();
-		for(int i=0;i<listofnodes.size();i++)
-		{
-			System.out.printf("%d , %d\n",i,listofnodes.size());
-			listofclients.add(listofnodes.get(i).Node);
-		}
 		try
 		{
 			File file=new File("C:/TEMP/JSONFile.json");
@@ -80,9 +85,11 @@ public class Nodelijst {
 			    JSONObject jsonobject = (JSONObject) jsonarray.get(i);
 			    String tempName = (String) jsonobject.get("name");
 			    String tempIpadress = (String) jsonobject.get("ipAdress");
-			    int tempHash = (int) jsonobject.get("hash");
 			    Nodelijst.addNode(tempName, tempIpadress);
 			}
+			System.out.println("\nreading JSON object from file");
+			System.out.println("-----------------------");
+			System.out.print(listofclients);
 					
 		} 
 		catch (FileNotFoundException e) { 
