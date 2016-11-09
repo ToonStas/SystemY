a845f984b681ed7d015243b6bd891f4a06bb3ee6
DESKTOP-IKJ3SVO\Thijs <Thijs_van_opstal@hotmail.com>
2016-11-09 12:10:37 +0100

    Multicast bijna af

--- SystemY/src/test2.java
+++ SystemY/src/test2.java
@@ -1,40 +1,101 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
 public class test2 {
 	
 	private int port = 8769; //Poort naarwaar we verzenden
 	private String group = "224.1.1.1";
-	//private int ttl = 1;		--> TTL weglaten?
 	private MulticastSocket s;
 	
 	public test2(){
+		String Name;
+		String Ipadres;
+		
+		Name = ReadConsoleNaam();
+		Ipadres =ReadConsoleIP();
 		try {
 			s = new MulticastSocket();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		sending();
+		sending(Name, Ipadres);
 		close();
 	}
 
-	public void sending(){
-		byte buf[] = new byte[10];
-	
-		for(int i=0 ; i<buf.length; i++){
-			buf[i] = (byte)i;
+	public String ReadConsoleNaam(){
+		String naam = null;
+		BufferedReader br = null;
+		
+		try{ 
+		br = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			System.out.println("Naam Node: ");
+			naam = br.readLine();
 		}
-		DatagramPacket pack = new DatagramPacket(buf, 10);
+		} catch(IOException e){
+			e.printStackTrace();
+		} finally{
+			if(br != null){
+				try{
+					br.close();
+				} catch(IOException e){
+				e.printStackTrace();
+				}
+			}
+		}
+		return naam;
+	}
+	
+	public String ReadConsoleIP(){
+		String IP = null;
+		BufferedReader br = null;
+		
+		try{ 
+		br = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			System.out.println("IP Node: ");
+			IP = br.readLine();
+		}
+		} catch(IOException e){
+			e.printStackTrace();
+		} finally{
+			if(br != null){
+				try{
+					br.close();
+				} catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		return IP;
+	}
+	
+	
+	public void sending(String naam, String Ip){
+		//Hier moeten we de n; het IP adres verzenden
+
+		String Naamip;
+		
+		Naamip = naam + Ip;
+		
+		byte[] sendData = Naamip.getBytes();	//Naam in sendData1 zetten
+		
+		for(int i=0 ; i<sendData.length; i++){
+			sendData[i] = (byte)i;
+		}
+		DatagramPacket pack = new DatagramPacket(sendData, 10);
 		try {
-			pack = new DatagramPacket(buf, buf.length,InetAddress.getByName(group), port);
+			pack = new DatagramPacket(sendData, sendData.length,InetAddress.getByName(group), port);
 		} catch (UnknownHostException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
 		try {
-			s.send(pack); 	//,(byte)ttl); --> TTL weglaten?
+			s.send(pack); 
 			System.out.println("Pakket is verzonden.");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -42,7 +103,6 @@ public class test2 {
 		}
 		
 	}
-	
 	public void close(){
 		s.close();
 	}	
