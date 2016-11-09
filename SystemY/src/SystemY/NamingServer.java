cb863adc2554acf84a372ca05e3d7c0b3e5b2ad5
Floris <floris.bosteels@gmail.com>
2016-11-09 11:52:12 +0100

    Fix bestandenlijst en nog wa

--- SystemY/src/SystemY/NamingServer.java
+++ SystemY/src/SystemY/NamingServer.java
@@ -2,14 +2,13 @@ package SystemY;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class NamingServer extends UnicastRemoteObject implements NamingServerInterface {
 	private static final long serialVersionUID = 1L;
 	private Nodelijst nodeLijst;
 
+	//Namingserver houdt enkel een lijst van nodes bij met hierin de naam, da hash en het ipadres. Niks meer!
 	public NamingServer() throws RemoteException{
 		super();
 		nodeLijst = new Nodelijst();
@@ -19,18 +18,19 @@ public class NamingServer extends UnicastRemoteObject implements NamingServerInt
 		nodeLijst.addNode("Matthias", "192.168.1.4");
 		//nodeLijst.writeJSON();
 		//nodeLijst.readJSON();
-
 	}
 	
 	public String getFileLocation(String fileName){
 		//TODO itereer door lijst met bestanden voor gekozen fileName en return dan het ipadres van de eigenaar
 		String location = "ipadres";
 		int hash = nodeLijst.calculateHash(fileName);
-		TreeMap<Integer, String> listOfNodes = new TreeMap<>();
+		hash = 0;
+		TreeMap<Integer, NodeNamingServer> listOfNodes = new TreeMap<>();
 		listOfNodes = nodeLijst.getListOfNodes();
 		
-		location = listOfNodes.floorEntry(hash).getValue();
+		location = listOfNodes.floorEntry(hash).getValue().getIpAdress(); //geeft het ipadres van de 1ste node <= de waarde van de hash
 		
+		System.out.println(location);
 		
 		return location;
 	}
