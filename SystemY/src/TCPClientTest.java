07e397e03d02eb16b43b610d6ed3baf137342041
Floris <floris.bosteels@gmail.com>
2016-11-14 12:39:10 +0100

    TCP test

---
+++ SystemY/src/TCPClientTest.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClientTest {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket(host, 4444);
+
+        File file = new File("C:/TEMP/testReceive.txt");
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+    }
+}
