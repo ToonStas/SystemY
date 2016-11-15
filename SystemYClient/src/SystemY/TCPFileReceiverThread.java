66a1e309885bf93961a43c7f8f37e0e16c8ab71d
LAPTOPTOON\Toon <toon.stas@student.uantwerpen.be>
2016-11-15 11:31:08 +0100

    Thread voor file receiving node

---
+++ SystemYClient/src/SystemY/TCPFileReceiverThread.java
@@ -0,0 +1,12 @@
+package SystemY;
+
+public class TCPFileReceiverThread implements Runnable {
+	
+	public void run() {
+		System.out.println("test");
+	}
+	
+	public static void main(String args[]){
+		(new Thread(new TCPFileReceiverThread))
+	}
+}
