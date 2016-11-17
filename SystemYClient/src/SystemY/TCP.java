b7fd88aeed358aef8d4bc04b0a4b8d4976db81e9
LAPTOPTOON\Toon <toon.stas@student.uantwerpen.be>
2016-11-17 23:33:32 +0100

    TCP client update

--- SystemYClient/src/SystemY/TCP.java
+++ SystemYClient/src/SystemY/TCP.java
@@ -1,20 +1,64 @@
 package SystemY;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCP {
-	public TCP(){
-		
+	public final static int SOCKET_PORT = 13267;
+	private Socket sock;
+	private ServerSocket serverSock;
+	private InetAddress ipPrevious;
+	private InetAddress ipNext;
+	private InetAddress ipNamingServer;
+	public TCP() throws IOException{
+		this.sock = new Socket();
+		try {
+			serverSock = new ServerSocket(SOCKET_PORT);
+		} catch (IOException e) {
+			System.out.println("Couldn't create serversocket");
+		}
+		this.listen();
 	}
 	
-	public void notifyNext(int previousHash, int nextHash, InetAddress ip)
+	public void notifyNextAdd(int previousHash, int nextHash, InetAddress ipNext)
+	{
+		checkSocket();
+	}
+	
+	public void notifyPreviousAdd(int previousHash, int nextHash, InetAddress ipPrevious)
 	{
 		
 	}
 	
-	public void notifyPrevious(int previousHash, int nextHash, InetAddress ip)
+	public void notifyNextShutdown(int previousHash, InetAddress ipPrevious)
 	{
 		
+	}
+	
+	public void notifyPreviousShutdown(int nextHash, InetAddress ipNext)
+	{
+		
+	}
+	
+	private void checkSocket()
+	{
+		if (sock.isClosed())
+		{
+			
+		}
+	}
+	
+	public void checkMessages()
+	{
+		
+	}
+	
+	public void listen() throws IOException
+	{
+		Thread listener = new Thread(new TCPServerSocketListener(SOCKET_PORT, this.sock, this.serverSock));
+		listener.start();
 	}
 
 }
--- SystemYClient/src/SystemY/TCPSocketHandler.java
+++ SystemYClient/src/SystemY/TCPSocketHandler.java
@@ -1,41 +1,91 @@
 package SystemY;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class TCPSocketHandler {
-	private int port;
-	private ServerSocket servSock = null;
-	private Socket sock = null;
+public class TCPSocketHandler extends Thread {
 	
-	public TCPSocketHandler(int socketPort){
-		port = socketPort;
-	}
-	public Socket getSocket()
+	private Socket sock;
+	public TCPSocketHandler (Socket sock)
 	{
-		if (sock.isClosed()){
-			try {
-				servSock = new ServerSocket(port);
-				try {
-					System.out.println("Attempting connection...");
-					
-					this.sock = servSock.accept();
-				} catch (IOException e) {
-					System.out.println("Could nog connect to socket.");
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Serversocket fail.");
+		this.sock = sock;
+	}
+	
+	public void run()
+	{
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(this.sock.getInputStream()));
+			String nextLine;
+			nextLine = in.readLine();
+			if (nextLine ==  "notifyNextAdd")
+			{
+				int previousHash = Integer.parseInt(in.readLine());
+				int nextHash = Integer.parseInt(in.readLine());
+				InetAddress ipNext = InetAddress.getByName(in.readLine());
+				
+				//action
+				System.out.println("notifyNextAdd message received: ");
+				System.out.println("------------------------------------");
+				System.out.println("Previous Hash: "+previousHash);
+				System.out.println("Next Hash: "+nextHash);
+				System.out.println("IP previous: "+ipNext.toString());
+			} 
+			else if (nextLine == "notifyPreviousAdd")
+			{
+				int previousHash = Integer.parseInt(in.readLine());
+				int nextHash = Integer.parseInt(in.readLine());
+				InetAddress ipPrevious = InetAddress.getByName(in.readLine());
+				
+				//action
+				System.out.println("notifyPreviousAdd message received: ");
+				System.out.println("------------------------------------");
+				System.out.println("Previous Hash: "+previousHash);
+				System.out.println("Next Hash: "+nextHash);
+				System.out.println("IP previous: "+ipPrevious.toString());
+				
 			}
+			else if (nextLine == "notifyNextShutdown")
+			{
+				int previousHash = Integer.parseInt(in.readLine());
+				InetAddress ipPrevious = InetAddress.getByName(in.readLine());
+				
+				//action
+				System.out.println("notifyNextShutdown message received: ");
+				System.out.println("------------------------------------");
+				System.out.println("Previous Hash: "+previousHash);
+				System.out.println("IP previous: "+ipPrevious.toString());
+			}
+			else if (nextLine == "notifyPreviousShutdown")
+			{
+				int nextHash = Integer.parseInt(in.readLine());
+				InetAddress ipNext = InetAddress.getByName(in.readLine());
+				
+				//action
+				System.out.println("notifyPreviousShutdown message received: ");
+				System.out.println("------------------------------------");
+				System.out.println("Next Hash: "+nextHash);
+				System.out.println("IP previous: "+ipNext.toString());
+			} 
+			else
+			{
+				System.out.println("Unsupported TCP-message");
+			}
+			in.close();
+			sock.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return sock;
 		
 		
 		
 	}
-	
 
 }
