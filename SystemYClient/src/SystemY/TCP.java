36b67d5739a2e316849d021338df10b98f6f7318
LAPTOPTOON\Toon <toon.stas@student.uantwerpen.be>
2016-11-18 00:05:43 +0100

    Update TCP client

--- SystemYClient/src/SystemY/TCP.java
+++ SystemYClient/src/SystemY/TCP.java
@@ -1,6 +1,8 @@
 package SystemY;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,10 +11,12 @@ public class TCP {
 	public final static int SOCKET_PORT = 13267;
 	private Socket sock;
 	private ServerSocket serverSock;
-	private InetAddress ipPrevious;
-	private InetAddress ipNext;
+	private InetAddress ipPreviousClient;
+	private InetAddress ipThisClient;
+	private InetAddress ipNextClient;
 	private InetAddress ipNamingServer;
 	public TCP() throws IOException{
+		ipThisClient = InetAddress.getLocalHost();
 		this.sock = new Socket();
 		try {
 			serverSock = new ServerSocket(SOCKET_PORT);
@@ -24,36 +28,108 @@ public class TCP {
 	
 	public void notifyNextAdd(int previousHash, int nextHash, InetAddress ipNext)
 	{
-		checkSocket();
-	}
-	
-	public void notifyPreviousAdd(int previousHash, int nextHash, InetAddress ipPrevious)
-	{
-		
-	}
-	
-	public void notifyNextShutdown(int previousHash, InetAddress ipPrevious)
-	{
-		
-	}
-	
-	public void notifyPreviousShutdown(int nextHash, InetAddress ipNext)
-	{
-		
-	}
-	
-	private void checkSocket()
-	{
-		if (sock.isClosed())
-		{
+		try {
+			if (sock.isClosed()!=true)
+			{
+				sock.close();
+			}
+			sock = new Socket(ipThisClient,SOCKET_PORT);
+			System.out.println("Connecting to server");
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+			out.write("notifyNextAdd");
+			out.newLine();
+			out.write(String.valueOf(previousHash));
+			out.newLine();
+			out.write(String.valueOf(nextHash));
+			out.newLine();
+			out.write(ipNext.toString());
+			out.flush();
+			out.close();
+			sock.close();
+			
+			
+		} catch (IOException e) {
 			
 		}
 	}
 	
-	public void checkMessages()
+	public void notifyPreviousAdd(int previousHash, int nextHash, InetAddress ipPrevious)
 	{
-		
+		try {
+			if (sock.isClosed()!=true)
+			{
+				sock.close();
+			}
+			sock = new Socket(ipThisClient,SOCKET_PORT);
+			System.out.println("Connecting to server");
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+			out.write("notifyPreviousAdd");
+			out.newLine();
+			out.write(String.valueOf(previousHash));
+			out.newLine();
+			out.write(String.valueOf(nextHash));
+			out.newLine();
+			out.write(ipPrevious.toString());
+			out.flush();
+			out.close();
+			sock.close();
+			
+			
+		} catch (IOException e) {
+			
+		}
 	}
+	
+	public void notifyNextShutdown(int previousHash, InetAddress ipPrevious)
+	{
+		try {
+			if (sock.isClosed()!=true)
+			{
+				sock.close();
+			}
+			sock = new Socket(ipThisClient,SOCKET_PORT);
+			System.out.println("Connecting to server");
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+			out.write("notifyNextShutdown");
+			out.newLine();
+			out.write(String.valueOf(previousHash));
+			out.newLine();
+			out.write(ipPrevious.toString());
+			out.flush();
+			out.close();
+			sock.close();
+			
+			
+		} catch (IOException e) {
+			
+		}
+	}
+	
+	public void notifyPreviousShutdown(int nextHash, InetAddress ipNext)
+	{
+		try {
+			if (sock.isClosed()!=true)
+			{
+				sock.close();
+			}
+			sock = new Socket(ipThisClient,SOCKET_PORT);
+			System.out.println("Connecting to server");
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+			out.write("notifyPreviousShutdown");
+			out.newLine();
+			out.write(String.valueOf(nextHash));
+			out.newLine();
+			out.write(ipNext.toString());
+			out.flush();
+			out.close();
+			sock.close();
+			
+			
+		} catch (IOException e) {
+			
+		}
+	}
+	
 	
 	public void listen() throws IOException
 	{
