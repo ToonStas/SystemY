fc074196bd21f4bbdaabca12ea3b2a6e7f7ce662
toons <toons@ToonTest.StasKot.be>
2016-12-26 17:53:22 +0100

    refactor ClientToClientInterface

--- SystemY/src/SystemY/ClientToClientInterface.java
+++ SystemY/src/SystemY/ClientToClientInterface.java
@@ -3,6 +3,6 @@ package SystemY;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface clientToClientInterface extends Remote {
+public interface ClientToClientInterface extends Remote {
 	public void getNotified(int previousHash, int nextHash) throws RemoteException;
 }
