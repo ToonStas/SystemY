94380dd29bc8afb9486263bc5925638336be2b5a
LAPTOPTOON\Toon <toon.stas@student.uantwerpen.be>
2017-01-02 15:59:48 +0100

    Revert "refactor ClientToClientInterface"
    This reverts commit fc074196bd21f4bbdaabca12ea3b2a6e7f7ce662.

--- SystemY/src/SystemY/clientToClientInterface.java
+++ SystemY/src/SystemY/clientToClientInterface.java
@@ -3,6 +3,6 @@ package SystemY;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface ClientToClientInterface extends Remote {
+public interface clientToClientInterface extends Remote {
 	public void getNotified(int previousHash, int nextHash) throws RemoteException;
 }
