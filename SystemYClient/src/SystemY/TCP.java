ef48ec0c278382784931307696818945629184eb
Matthias <matthias.bernaerts@outlook.com>
2017-01-09 20:17:01 +0100

    .

--- SystemYClient/src/SystemY/TCP.java
+++ SystemYClient/src/SystemY/TCP.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
-// this class handles the tcp sends/receives.
+// this class handles the tcp sends/receives
 public class TCP {
 	private final static int SOCKET_PORT = 13267;
 	private Thread receiveThread;
